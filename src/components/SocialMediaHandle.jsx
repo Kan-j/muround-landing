70daae373711ee85ab05e636adf19e51bb940098
Kan-j <kwasiaddonyarkojunior@gmail.com>
2024-02-10 20:00:31 +0000

    social-media update

--- src/components/SocialMediaHandle.jsx
+++ src/components/SocialMediaHandle.jsx
@@ -4,12 +4,12 @@ import { FaInstagram, FaLinkedin,FaFacebook, FaTwitter } from "react-icons/fa";
 const SocialMediaHandle = () => {
   return (
     <section className='grid mt-7 mb-6 justify-items-center grid-cols-2'>
-        <p className='font-medium text-xl'>Connect with us</p>
+        <p className='font-medium text-base md:text-xl'>Connect with us</p>
         <article className='flex justify-between w-2/4'>
-            <a href="" className='link text-accent'> <FaInstagram fontSize={30}/></a>
-            <a href="" className='link text-accent'> <FaTwitter fontSize={30}/></a>
-            <a href="" className='link text-accent'> <FaFacebook fontSize={30}/></a>
-            <a href="" className='link text-accent'> <FaLinkedin fontSize={30}/></a>
+            <a href="" className='link text-accent'> <FaInstagram fontSize={24}/></a>
+            <a href="" className='link text-accent'> <FaTwitter fontSize={24}/></a>
+            <a href="" className='link text-accent'> <FaFacebook fontSize={24}/></a>
+            <a href="" className='link text-accent'> <FaLinkedin fontSize={24}/></a>
         </article>
 
     </section>
